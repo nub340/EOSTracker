8df4c166cd4679ccbe1d21595008e6bf5d9f9b25
nub340 <nkoier@gmail.com>
2018-07-30 15:25:22 -0700

    merged

--- protractor.conf.js
+++ protractor.conf.js
@@ -6,23 +6,30 @@ const { SpecReporter } = require('jasmine-spec-reporter');
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './e2e/**/*.e2e-spec.ts'
+    './e2e/specs/sidebar-spec.ts'
   ],
   capabilities: {
     'browserName': 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
+  params: {
+    breakpoints: {
+      normal: { width: 1920, height: 1080 },
+      mobile: { width: 720, height: 900 }
+    }
+  },
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () { }
   },
   onPrepare() {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
+    //browser.ignoreSynchronization = true;
     jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
